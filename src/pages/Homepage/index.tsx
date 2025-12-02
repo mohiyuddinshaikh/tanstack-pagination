2746fcdeab649c33b687c54e9c17e60d656c8439
Mohiyuddin Shaikh <mohiyuddin.shaikh46@gmail.com>
2025-12-02 15:18:58 +0530

    uninstalled shadcn for minimal requirement

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@/components/ui/button";
 import "./Homepage.scss";
 import { useNavigate } from "react-router";
 
@@ -12,9 +11,7 @@ export default function Homepage() {
   return (
     <div className="homepage">
       <h2>Tanstack Pagination Homepage</h2>
-      <Button variant="secondary" onClick={viewProducts}>
-        View Products
-      </Button>
+      <button onClick={viewProducts}>View Products</button>
     </div>
   );
 }
